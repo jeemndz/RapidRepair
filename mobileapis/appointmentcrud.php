bb395ec54ea8a35f6bea0311fd70f72b164a6fb3
jiem <johnlurjmendoza1@gmail.com>
2026-04-06 00:51:27 +0800

    changes to be committed:

--- mobileapis/appointmentcrud.php
+++ mobileapis/appointmentcrud.php
@@ -9,7 +9,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
   exit;
 }
 
-require_once 'config.php';
+require_once __DIR__ . '/db.php';
 
 $conn = new mysqli(
   'rapidrepairs.mysql.database.azure.com',
