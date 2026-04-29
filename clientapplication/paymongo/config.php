5648c47e0afa767218160bb6e6e7f03d9d237356
jiem <johnlurjmendoza1@gmail.com>
2026-04-30 02:49:35 +0800

    added azure base URL

--- clientapplication/paymongo/config.php
+++ clientapplication/paymongo/config.php
@@ -1,5 +1,16 @@
 <?php
 
-$PAYMONGO_SECRET_KEY = "sk_test_Z6ynXiE7Ywm2KmTCoKsHZnz2"; // your PayMongo secret key
+// Secret key (use env if available)
+$PAYMONGO_SECRET_KEY = getenv("PAYMONGO_SECRET_KEY") ?: "sk_test_Z6ynXiE7Ywm2KmTCoKsHZnz2";
 
-$BASE_URL = "http://localhost/RapidRepair/clientapplication/paymongo";
+// Detect if running on localhost or Azure
+$host = $_SERVER['HTTP_HOST'] ?? '';
+
+// LOCAL
+if (strpos($host, 'localhost') !== false || strpos($host, '127.0.0.1') !== false) {
+    $BASE_URL = "http://localhost/RapidRepair/clientapplication/paymongo";
+} 
+// AZURE (LIVE / DEPLOYED)
+else {
+    $BASE_URL = "https://rapidrepair-gygpcbczygy0czek.southeastasia-01.azurewebsites.net/clientapplication/paymongo";
+}
